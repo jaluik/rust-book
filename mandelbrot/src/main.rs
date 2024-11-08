87b3a4ac73afdad35fea6f802f113c99f46ea4e0
jaluik <cqzhengmouren@gmail.com>
2024-11-08 12:27:54 +0800

    feat: complex

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -33,3 +33,17 @@ fn test_parser_pair() {
     assert_eq!(parse_pair::<i32>("10,20", ','), Some((10, 20)));
     assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
 }
+
+
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some(Complex{re, im}),
+        None => None
+    }
+}
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(parse_complex("1.25,-0.0625"), Some(Complex{re: 1.25, im: -0.0625}));
+    assert_eq!(parse_complex(",-0.0625"), None);
+}
